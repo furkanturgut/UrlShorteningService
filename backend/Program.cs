ddbf45f242b4d1ff3560bde94c5f42dc9df619fa
Furkan Turgut <153141827+furkanturgut@users.noreply.github.com>
2025-05-11 22:09:47 +0300

    Create Program.cs

---
+++ backend/Program.cs
@@ -0,0 +1,35 @@
+using backend.Data;
+using backend.Interfaces;
+using backend.Repositories;
+using backend.Services;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDataContext>(options=> 
+{
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+});
+builder.Services.AddScoped<IUrlRepository, UrlRepository>();
+builder.Services.AddScoped<IUrlService, UrlService>();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();  
+app.MapControllers();
+
+app.Run();
+
+
